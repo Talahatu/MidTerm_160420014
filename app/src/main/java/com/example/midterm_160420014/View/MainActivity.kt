7c9155650fda28885d91b913fa41bdfa7b95044d
Talahatu <87047371+Talahatu@users.noreply.github.com>
2023-04-06 18:31:22 +0700

    some fragment

--- app/src/main/java/com/example/midterm_160420014/View/MainActivity.kt
+++ app/src/main/java/com/example/midterm_160420014/View/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.midterm_160420014
+package com.example.midterm_160420014.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.midterm_160420014.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
