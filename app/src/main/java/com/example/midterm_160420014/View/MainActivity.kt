c343ec8594e0c445c9386aac873a04fb243435d8
Talahatu <87047371+Talahatu@users.noreply.github.com>
2023-04-26 00:39:41 +0700

    Drawer Layout (On Progress)

--- app/src/main/java/com/example/midterm_160420014/View/MainActivity.kt
+++ app/src/main/java/com/example/midterm_160420014/View/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.midterm_160420014.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.midterm_160420014.R
 
 class MainActivity : AppCompatActivity() {
@@ -10,4 +12,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = this.findNavController(R.id.hostFragment)
+//        return navController.navigateUp()
+//    }
 }
